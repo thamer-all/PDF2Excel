3deb955cb4628dd8ecf34168ce772b68547a572a
Thamer Allahabi <thamer.a.allahabi@gmail.com>
2019-07-13 08:57:09 -0400

    Update Program.cs

--- PDF2Excel/Program.cs
+++ PDF2Excel/Program.cs
@@ -8,9 +8,6 @@ namespace PDF2Excel
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
